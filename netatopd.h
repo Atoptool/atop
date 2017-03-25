5f101e656a24271726d1e9cd672631b6033c36c1
Gerlof Langeveld <gerlof.langeveld@atoptool.nl>
2017-03-25 12:40:23 +0100

    Preparations to move atop to github

--- netatopd.h
+++ netatopd.h
@@ -1,1 +1,12 @@
-../Netatop/netatopd.h
+#define SEMAKEY         1541961
+
+#define NETEXITFILE     "/var/run/netatop.log"
+#define MYMAGIC         (unsigned int) 0xfeedb0b0
+
+struct naheader {
+        u_int32_t	magic;	// magic number MYMAGIC
+        u_int32_t	curseq;	// sequence number of last netpertask
+        u_int16_t	hdrlen;	// length of this header
+        u_int16_t	ntplen;	// length of netpertask structure
+        pid_t    	mypid;	// PID of netatopd itself
+};
