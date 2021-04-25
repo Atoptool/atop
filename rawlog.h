d2ca9f2954a25b4955c661bb892817988fd03b2e
Gerlof Langeveld <gerlof.langeveld@atoptool.nl>
2021-04-25 14:21:38 +0200

    Add license info to various include files (solves issue #144)

--- rawlog.h
+++ rawlog.h
@@ -1,4 +1,32 @@
 /*
+** ATOP - System & Process Monitor
+**
+** The program 'atop' offers the possibility to view the activity of 
+** the system on system-level as well as process-level.
+** ==========================================================================
+** Author:      Gerlof Langeveld
+** E-mail:      gerlof.langeveld@atoptool.nl
+** Date:        September 2002
+** --------------------------------------------------------------------------
+** Copyright (C) 2000-2010 Gerlof Langeveld
+**
+** This program is free software; you can redistribute it and/or modify it
+** under the terms of the GNU General Public License as published by the
+** Free Software Foundation; either version 2, or (at your option) any
+** later version.
+**
+** This program is distributed in the hope that it will be useful, but
+** WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+** --------------------------------------------------------------------------
+*/
+
+/*
 ** structure describing the raw file contents
 **
 ** layout raw file:    rawheader
