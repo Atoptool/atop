Atoptool/atop

commit 1fbda3f1081a3c2734a215082ff2d4f1870e5898

Additions to NUMA counters Table overflow checks, bug fix and manual page has been added.

deviate.c
@@ -715,6 +715,7 @@ deviatsyst(struct sstat *cur, struct sstat *pre, struct sstat *dev,
 	dev->mem.pgmigrate	= subcount(cur->mem.pgmigrate,   pre->mem.pgmigrate);
 
 	dev->memnuma.nrnuma     = cur->memnuma.nrnuma;
+
 	for (i=0; i < dev->memnuma.nrnuma; i++)
 	{
 		dev->memnuma.numa[i].totmem      = cur->memnuma.numa[i].totmem;
@@ -728,15 +729,14 @@ deviatsyst(struct sstat *cur, struct sstat *pre, struct sstat *dev,
 		dev->memnuma.numa[i].slabreclaim = cur->memnuma.numa[i].slabreclaim;
 		dev->memnuma.numa[i].tothp       = cur->memnuma.numa[i].tothp;
 		dev->memnuma.numa[i].frag        = cur->memnuma.numa[i].frag;
-		dev->memnuma.numa[i].numanr      = cur->memnuma.numa[i].numanr;
 	}
 
-	if (cur->cpunuma.nrnuma > 1)
+	dev->cpunuma.nrnuma = cur->cpunuma.nrnuma;
+
+	if (dev->cpunuma.nrnuma > 1)
 	{
-		dev->cpunuma.nrnuma     = cur->cpunuma.nrnuma;
 		for (i=0; i < dev->cpunuma.nrnuma; i++)
 		{
-			dev->cpunuma.numa[i].numanr = cur->cpunuma.numa[i].numanr;
 			dev->cpunuma.numa[i].utime  = subcount(cur->cpunuma.numa[i].utime,
 								pre->cpunuma.numa[i].utime);
 			dev->cpunuma.numa[i].ntime  = subcount(cur->cpunuma.numa[i].ntime,
@@ -758,7 +758,7 @@ deviatsyst(struct sstat *cur, struct sstat *pre, struct sstat *dev,
 		}
 	}
 
-	dev->psi          	= cur->psi;
+	dev->psi = cur->psi;
 
 	if (cur->psi.present)
 	{

photosyst.c
@@ -199,7 +199,7 @@ static struct v6tab 		v6tab[] = {
 
 static int	v6tab_entries = sizeof(v6tab)/sizeof(struct v6tab);
 
-// The following are used to accumulate cpu statistics for per numa.
+// The following values are used to accumulate cpu statistics per numa.
 // The bitmask realization is from numactl
 #define CPUMASK_SZ (64 * 8)
 
@@ -460,7 +460,7 @@ photosyst(struct sstat *si)
             {
                 long long f=0;
 
-                sprintf(fn,
+                snprintf(fn, sizeof fn,
                    "/sys/devices/system/cpu/cpu%d/cpufreq/stats/time_in_state",
                    i);
 
@@ -490,7 +490,7 @@ photosyst(struct sstat *si)
                 }
 		else
 		{    // governor statistics not available
-                     sprintf(fn,  
+                     snprintf(fn, sizeof fn, 
                       "/sys/devices/system/cpu/cpu%d/cpufreq/cpuinfo_max_freq",
 		      i);
 
@@ -509,7 +509,7 @@ photosyst(struct sstat *si)
 	                        si->cpu.cpu[i].freqcnt.maxfreq=0;
                         }
 
-                       sprintf(fn,  
+                       snprintf(fn, sizeof fn,
                        "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_cur_freq",
 		       i);
         
@@ -841,6 +841,7 @@ photosyst(struct sstat *si)
 	** /sys/devices/system/node/node0/meminfo, and store them in binary form.
 	*/
 	dirp = opendir(NUMADIR);
+
 	if (dirp)
 	{
 		/*
@@ -850,11 +851,17 @@ photosyst(struct sstat *si)
 		{
 			if (strncmp(dentry->d_name, "node", 4))
 				continue;
+
 			j = strtoul(dentry->d_name + 4, NULL, 0);
+
+			if (j >= MAXNUMA)	// too many NUMA nodes?
+				continue;	// skip (no break, because order unknown)
+
 			si->memnuma.nrnuma++;
 
-			sprintf(fn, "/sys/devices/system/node/node%d/meminfo", j);
-			if ( (fp = fopen(fn, "r")) != 0)
+			snprintf(fn, sizeof fn, NUMADIR "/%s/meminfo", dentry->d_name);
+
+			if ( (fp = fopen(fn, "r")) != NULL)
 			{
 				while ( fgets(linebuf, sizeof(linebuf), fp) != NULL)
 				{
@@ -886,7 +893,6 @@ photosyst(struct sstat *si)
 					else if ( strcmp("HugePages_Total:", nam) == EQ)
 						si->memnuma.numa[j].tothp = cnts[1];
 				}
-				si->memnuma.numa[j].numanr = j;
 				fclose(fp);
 			}
 		}
@@ -898,6 +904,7 @@ photosyst(struct sstat *si)
 	{
 		char tmp[64];
 		float frag[MAX_ORDER];
+
 		/* If kernel CONFIG_COMPACTION is enabled, get the percentage directly */
 		if ( (fp = fopen("/sys/kernel/debug/extfrag/unusable_index", "r")) != NULL )
 		{
@@ -954,7 +961,9 @@ photosyst(struct sstat *si)
 						prev_free += free_page[j];
 					total_frag += (float)prev_free/total_free;
 				}
-				si->memnuma.numa[cnts[0]].frag = total_frag/MAX_ORDER;
+
+				if (cnts[0] < MAXNUMA)
+					si->memnuma.numa[cnts[0]].frag = total_frag/MAX_ORDER;
 			}
 			fclose(fp);
 		}
@@ -970,49 +979,49 @@ photosyst(struct sstat *si)
 		size_t len = 0;
 		struct bitmask *mask;
 
-		dirp = opendir(NUMADIR);
-		if (dirp)
+		mask = numa_allocate_cpumask();
+
+		si->cpunuma.nrnuma = si->memnuma.nrnuma;
+
+		for (j=0; j < si->cpunuma.nrnuma; j++)
 		{
-			mask = numa_allocate_cpumask();
-			while ( (dentry = readdir(dirp)) )
+			snprintf(fn, sizeof fn, NUMADIR "/node%d/cpumap", j);
+
+			if ( (fp = fopen(fn, "r")) != 0)
 			{
-				if (strncmp(dentry->d_name, "node", 4))
-					continue;
-				j = strtoul(dentry->d_name + 4, NULL, 0);
-				si->cpunuma.nrnuma++;
-
-				sprintf(fn, "%s/node%d/cpumap", NUMADIR, j);
-				if ( (fp = fopen(fn, "r")) != 0)
+				if ( getdelim(&line, &len, '\n', fp) > 0 )
 				{
-					if ( getdelim(&line, &len, '\n', fp) > 0 )
-						if (numa_parse_bitmap_v2(line, mask) < 0)
-						{
-							mcleanstop(54, "failed to parse numa bitmap\n");
-						}
-					fclose(fp);
-				}
-
-				for (i = 0; i < mask->size; i++)
-				{
-					if ( (mask->maskp[i/bitsperlong] >> (i % bitsperlong)) & 1 )
+					if (numa_parse_bitmap_v2(line, mask) < 0)
 					{
-						si->cpunuma.numa[j].utime += si->cpu.cpu[i].utime;
-						si->cpunuma.numa[j].ntime += si->cpu.cpu[i].ntime;
-						si->cpunuma.numa[j].stime += si->cpu.cpu[i].stime;
-						si->cpunuma.numa[j].itime += si->cpu.cpu[i].itime;
-						si->cpunuma.numa[j].wtime += si->cpu.cpu[i].wtime;
-						si->cpunuma.numa[j].Itime += si->cpu.cpu[i].Itime;
-						si->cpunuma.numa[j].Stime += si->cpu.cpu[i].Stime;
-						si->cpunuma.numa[j].steal += si->cpu.cpu[i].steal;
-						si->cpunuma.numa[j].guest += si->cpu.cpu[i].guest;
+						mcleanstop(54, "failed to parse numa bitmap\n");
 					}
 				}
-				si->cpunuma.numa[j].numanr = j;
+				fclose(fp);
 			}
-			free(line);
-			numa_bitmask_free(mask);
-			closedir(dirp);
+
+			for (i=0; i < mask->size; i++)
+			{
+				if ( (mask->maskp[i/bitsperlong] >> (i % bitsperlong)) & 1 )
+				{
+					si->cpunuma.numa[j].utime += si->cpu.cpu[i].utime;
+					si->cpunuma.numa[j].ntime += si->cpu.cpu[i].ntime;
+					si->cpunuma.numa[j].stime += si->cpu.cpu[i].stime;
+					si->cpunuma.numa[j].itime += si->cpu.cpu[i].itime;
+					si->cpunuma.numa[j].wtime += si->cpu.cpu[i].wtime;
+					si->cpunuma.numa[j].Itime += si->cpu.cpu[i].Itime;
+					si->cpunuma.numa[j].Stime += si->cpu.cpu[i].Stime;
+					si->cpunuma.numa[j].steal += si->cpu.cpu[i].steal;
+					si->cpunuma.numa[j].guest += si->cpu.cpu[i].guest;
+				}
+			}
 		}
+
+		free(line);
+		numa_bitmask_free(mask);
+	}
+	else
+	{
+		si->cpunuma.nrnuma = 0;
 	}
 
 	/*

photosyst.h
@@ -90,7 +90,6 @@ struct	memstat {
 /************************************************************************/
 
 struct	mempernuma {
-	int	numanr;
 	count_t	totmem;		// number of physical pages for this numa
 	count_t	freemem;	// number of free     pages for this numa
 	count_t	filepage;	// number of file     pages for this numa
@@ -115,7 +114,6 @@ struct	memnuma {
 };
 
 struct	cpupernuma {
-	int	numanr;
 	count_t	stime;		// accumulate system  time in clock ticks for per numa
 	count_t	utime;		// accumulate user    time in clock ticks for per numa
 	count_t	ntime;		// accumulate nice    time in clock ticks for per numa

showsys.c
@@ -1513,6 +1513,16 @@ sysval_KSMSHARED(struct sstat *sstat)
 sys_printdef syspdef_KSMSHARED = {"KSMSHARED", sysprt_KSMSHARED, sysval_KSMSHARED};
 /*******************************************************************/
 static char *
+sysprt_NUMNUMA(struct sstat *sstat, extraparam *notused, int badness, int *color) 
+{
+        static char buf[16]="numnode  ";
+        val2valstr(sstat->memnuma.nrnuma, buf+8, 4, 0, 0);
+        return buf;
+}
+
+sys_printdef syspdef_NUMNUMA = {"NUMNUMA", sysprt_NUMNUMA, NULL};
+/*******************************************************************/
+static char *
 sysprt_SWPCOMMITTED(struct sstat *sstat, extraparam *notused, int badness, int *color) 
 {
         static char buf[16]="vmcom  ";
@@ -1686,7 +1696,7 @@ sysprt_NUMANR(struct sstat *sstat, extraparam *as, int badness, int *color)
 {
 	static char buf[16];
 	*color = -1;
-	sprintf(buf, "     numa%03d", sstat->memnuma.numa[as->index].numanr);
+	sprintf(buf, "numanode%04d", as->index);
 	return buf;
 }
 
@@ -1879,8 +1889,8 @@ sysprt_NUMACPUWAIT(struct sstat *sstat, extraparam *as, int badness, int *color)
 {
 	static char buf[15];
 
-	sprintf(buf, "numa%02d w%3.0f%%",
-		sstat->cpunuma.numa[as->index].numanr,
+	sprintf(buf, "nod%03d w%3.0f%%",
+		as->index,
 		(sstat->cpunuma.numa[as->index].wtime * 100.0) / as->percputot);
 	return buf;
 }

man/atop.1
@@ -1208,7 +1208,8 @@ balloon driver (`vmbal`),
 the amount of memory that is currently claimed by the ARC (cache)
 of ZFSonlinux (`zfarc`),
 the amount of memory that is claimed for huge pages (`hptot`),
-and the amount of huge page memory that is really in use (`hpuse`).
+the amount of huge page memory that is really in use (`hpuse`),
+and the number of NUMA nodes in this system (`numnode').
 
 If the screen-width does not allow all of these counters,
 only a relevant subset is shown.
@@ -1232,6 +1233,29 @@ The committed space is the reserved virtual space for all allocations of
 private memory space for processes. The kernel only verifies whether the
 committed space exceeds the limit if strict overcommit handling is
 configured (vm.overcommit_memory is 2).
+.PP
+.TP 5
+.B NUM
+Memory utilization per NUMA node.
+.br
+This line shows the total amount of physical memory of this node (`tot'),
+the amount of free memory (`free'), the amount of memory for cached file data
+(`file'), modified cached file data (`dirty'), recently used memory (`activ'),
+less recently used memory (`inact'), memory being used for kernel mallocs
+(`slab'), the amount of slab memory that is reclaimable (`slrec'),
+shared memory including tmpfs (`shmem'), total huge pages (`hptot') and
+the fragmentation percentage (`frag').
+.PP
+.TP 5
+.B NUC
+CPU utilization per NUMA node (not shown in case of a single NUMA node).
+.br
+This line shows the utilization percentages of all CPUs related to this
+NUMA node, categorized for  system mode (`sys'), user mode (`user'),
+user mode for niced processes (`niced'), idle mode (`idle'),
+wait mode (`w' preceded by the node number), irq mode (`irq'),
+softirq mode (`sirq'), steal mode (`steal'), and guest mode (`guest')
+overlapping user mode.
 .PP
 .TP 5
 .B PAG

showlinux.c
@@ -390,6 +390,7 @@ sys_printdef *memsyspdefs[] = {
 	&syspdef_BLANKBOX,
 	&syspdef_HUPTOT,
 	&syspdef_HUPUSE,
+	&syspdef_NUMNUMA,
         0
 };
 sys_printdef *swpsyspdefs[] = {
@@ -1141,7 +1142,8 @@ pricumproc(struct sstat *sstat, struct devtstat *devtstat,
 	                "ZFSARC:6 "
 	                "BLANKBOX:0 "
 	                "HUPTOT:6 "
-	                "HUPUSE:3 ",
+	                "HUPUSE:3 "
+	                "NUMNUMA:7 ",
 			memsyspdefs, "builtin memline",
 			sstat, &extra);
                 }
@@ -2076,8 +2078,7 @@ prisyst(struct sstat *sstat, int curline, int nsecs, int avgval,
 			if (screen)
 				move(curline, 0);
 
-			showsysline(memnumaline, sstat, &extra, "MPN",
-								badness);
+			showsysline(memnumaline, sstat, &extra, "NUM", badness);
 			curline++;
 			lin++;
 		}
@@ -2092,7 +2093,8 @@ prisyst(struct sstat *sstat, int curline, int nsecs, int avgval,
 		     extra.index < sstat->cpunuma.nrnuma && lin < maxnumalines;
 		     extra.index++)
 		{
-			extra.pernumacputot = sstat->cpunuma.numa[extra.index].utime +
+			extra.pernumacputot = sstat->cpunuma.numa[extra.index].stime +
+					      sstat->cpunuma.numa[extra.index].utime +
 					      sstat->cpunuma.numa[extra.index].ntime +
 					      sstat->cpunuma.numa[extra.index].itime +
 					      sstat->cpunuma.numa[extra.index].wtime +
@@ -2133,8 +2135,7 @@ prisyst(struct sstat *sstat, int curline, int nsecs, int avgval,
 			if (screen)
 				move(curline, 0);
 
-			showsysline(cpunumaline, sstat, &extra, "CPN",
-								badness);
+			showsysline(cpunumaline, sstat, &extra, "NUC", badness);
 			curline++;
 			lin++;
 		}

showlinux.h
@@ -213,6 +213,7 @@ extern sys_printdef syspdef_KSMSHARING;
 extern sys_printdef syspdef_KSMSHARED;
 extern sys_printdef syspdef_SWPCOMMITTED;
 extern sys_printdef syspdef_SWPCOMMITLIM;
+extern sys_printdef syspdef_NUMNUMA;
 extern sys_printdef syspdef_NUMANR;
 extern sys_printdef syspdef_NUMATOT;
 extern sys_printdef syspdef_NUMAFREE;

commit 2b07b014153b6ccb00a4e02b134fa6cc9cec6fc2

photosys.c: correct the numa number for NUM and NUC
Current code uses array index to store the numa number, which may
sort the numanr to a wrong value after the qsort() funtion in
generic_samp().

Fix this by re-adding the 'numanr' member for struct mempernuma and
cpupernuma to store the correct numa number. The permanent 'numanr'
member can guarantee the numa number from adjusting after qsort(),
while the qsort() function can continue working by sorting NUM from
small to large according to free memory, and sorting NUC from small
to large according to idle cpu.

Besides, add NUC for JSON output.

Fixes: 1fbda3f1081a ("Additions to NUMA counters")
Reported-by: Qi Zheng <zhengqi.arch@bytedance.com>
Signed-off-by: Fei Li <lifei.shirley@bytedance.com>

deviate.c
@@ -742,6 +742,7 @@ deviatsyst(struct sstat *cur, struct sstat *pre, struct sstat *dev,
 
 	for (i=0; i < dev->memnuma.nrnuma; i++)
 	{
+		dev->memnuma.numa[i].numanr      = cur->memnuma.numa[i].numanr;
 		dev->memnuma.numa[i].totmem      = cur->memnuma.numa[i].totmem;
 		dev->memnuma.numa[i].freemem     = cur->memnuma.numa[i].freemem;
 		dev->memnuma.numa[i].filepage    = cur->memnuma.numa[i].filepage;
@@ -762,6 +763,7 @@ deviatsyst(struct sstat *cur, struct sstat *pre, struct sstat *dev,
 		for (i=0; i < dev->cpunuma.nrnuma; i++)
 		{
 			dev->cpunuma.numa[i].nrcpu  = cur->cpunuma.numa[i].nrcpu;
+			dev->cpunuma.numa[i].numanr = cur->cpunuma.numa[i].numanr;
 
 			dev->cpunuma.numa[i].utime  = subcount(cur->cpunuma.numa[i].utime,
 								pre->cpunuma.numa[i].utime);

json.c
@@ -70,6 +70,7 @@ static void json_print_NFS();
 static void json_print_NET();
 static void json_print_IFB();
 static void json_print_NUM();
+static void json_print_NUC();
 static void json_print_LLC();
 static void json_print_PRG();
 static void json_print_PRC();
@@ -106,6 +107,7 @@ static struct labeldef	labeldef[] = {
 	{ "NET",	0,	json_print_NET },
 	{ "IFB",	0,	json_print_IFB },
 	{ "NUM",	0,	json_print_NUM },
+	{ "NUC",	0,	json_print_NUC },
 	{ "LLC",	0,	json_print_LLC },
 
 	{ "PRG",	0,	json_print_PRG },
@@ -813,13 +815,14 @@ static void json_print_NUM(char *hp, struct sstat *ss, struct tstat *ps, int nac
 {
 	register int i;
 
-        printf(", %s: [", hp);
+	printf(", %s: [", hp);
 
 	for (i = 0; i < ss->memnuma.nrnuma; i++) {
 		if (i > 0) {
 			printf(", ");
 		}
-		printf("{\"frag\": \"%f\", "
+		printf("{\"numanr\": \"%d\", "
+			"\"frag\": \"%f\", "
 			"\"totmem\": %lld, "
 			"\"freemem\": %lld, "
 			"\"active\": %lld, "
@@ -830,6 +833,7 @@ static void json_print_NUM(char *hp, struct sstat *ss, struct tstat *ps, int nac
 			"\"slabreclaim\": %lld, "
 			"\"shmem\": %lld, "
 			"\"tothp\": %lld}",
+			ss->memnuma.numa[i].numanr,
 			ss->memnuma.numa[i].frag * 100.0,
 			ss->memnuma.numa[i].totmem * pagesize,
 			ss->memnuma.numa[i].freemem * pagesize,
@@ -841,6 +845,41 @@ static void json_print_NUM(char *hp, struct sstat *ss, struct tstat *ps, int nac
 			ss->memnuma.numa[i].slabreclaim * pagesize,
 			ss->memnuma.numa[i].shmem * pagesize,
 			ss->memnuma.numa[i].tothp * ss->mem.hugepagesz);
+	}
+
+	printf("]");
+}
+
+static void json_print_NUC(char *hp, struct sstat *ss, struct tstat *ps, int nact)
+{
+	register int i;
+
+	printf(", %s: [", hp);
+
+	for (i = 0; i < ss->cpunuma.nrnuma; i++) {
+		if (i > 0) {
+			printf(", ");
+		}
+		printf("{\"numanr\": \"%d\", "
+			"\"stime\": %lld, "
+			"\"utime\": %lld, "
+			"\"ntime\": %lld, "
+			"\"itime\": %lld, "
+			"\"wtime\": %lld, "
+			"\"Itime\": %lld, "
+			"\"Stime\": %lld, "
+			"\"steal\": %lld, "
+			"\"guest\": %lld}",
+			ss->cpunuma.numa[i].numanr,
+			ss->cpunuma.numa[i].stime,
+			ss->cpunuma.numa[i].utime,
+			ss->cpunuma.numa[i].ntime,
+			ss->cpunuma.numa[i].itime,
+			ss->cpunuma.numa[i].wtime,
+			ss->cpunuma.numa[i].Itime,
+			ss->cpunuma.numa[i].Stime,
+			ss->cpunuma.numa[i].steal,
+			ss->cpunuma.numa[i].guest);
 	}
 
 	printf("]");

parseable.c
@@ -657,7 +657,7 @@ print_NUM(char *hp, struct sstat *ss, struct tstat *ps, int nact)
 	for (i=0; i < ss->memnuma.nrnuma; i++)
 	{
 		printf(	"%s %d %u %.0f %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld\n",
-			hp, i,
+			hp, ss->memnuma.numa[i].numanr,
 			pagesize,
 			ss->memnuma.numa[i].frag * 100.0,
 			ss->memnuma.numa[i].totmem,
@@ -681,7 +681,7 @@ print_NUC(char *hp, struct sstat *ss, struct tstat *ps, int nact)
 	for (i=0; i < ss->cpunuma.nrnuma; i++)
 	{
 		printf(	"%s %d %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld\n",
-			hp, i,
+			hp, ss->cpunuma.numa[i].numanr,
 	        	ss->cpunuma.numa[i].nrcpu,
 	        	ss->cpunuma.numa[i].stime,
         		ss->cpunuma.numa[i].utime,

photosyst.c
@@ -875,6 +875,7 @@ photosyst(struct sstat *si)
 					if (cnts[0] != j)
 						continue;
 
+					si->memnuma.numa[j].numanr = j;
 					if ( strcmp("MemTotal:", nam) == EQ)
 						si->memnuma.numa[j].totmem = cnts[1]*1024/pagesize;
 					else if ( strcmp("MemFree:", nam) == EQ)
@@ -1018,6 +1019,7 @@ photosyst(struct sstat *si)
 					si->cpunuma.numa[j].guest += si->cpu.cpu[i].guest;
 				}
 			}
+			si->cpunuma.numa[j].numanr = j;
 		}
 
 		free(line);

photosyst.h
@@ -96,6 +96,7 @@ struct	memstat {
 /************************************************************************/
 
 struct	mempernuma {
+	int	numanr;
 	float	frag;		// fragmentation level for this numa
 	count_t	totmem;		// number of physical pages for this numa
 	count_t	freemem;	// number of free     pages for this numa
@@ -117,6 +118,7 @@ struct	memnuma {
 };
 
 struct	cpupernuma {
+	int	numanr;
 	count_t	nrcpu;		// number of cpu's
 	count_t	stime;		// accumulate system  time in clock ticks for per numa
 	count_t	utime;		// accumulate user    time in clock ticks for per numa

showsys.c
@@ -1765,7 +1765,7 @@ sysprt_NUMANR(struct sstat *sstat, extraparam *as, int badness, int *color)
 {
 	static char buf[16];
 	*color = -1;
-	sprintf(buf, "numanode%04d", as->index);
+	sprintf(buf, "numanode%04d", sstat->memnuma.numa[as->index].numanr);
 	return buf;
 }
 
@@ -1970,7 +1970,7 @@ sysprt_NUMACPUWAIT(struct sstat *sstat, extraparam *as, int badness, int *color)
 	static char buf[15];
 
 	sprintf(buf, "nod%03d w%3.0f%%",
-		as->index,
+		sstat->cpunuma.numa[as->index].numanr,
 		(sstat->cpunuma.numa[as->index].wtime * 100.0) / as->percputot);
 	return buf;
 }